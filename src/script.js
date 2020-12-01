334b52551dcfb88a86674ea7411f6ca81deebabb
gcairesdev <pessoalgcaires@gmail.com>
2020-11-30 22:17:11 -0300

    :sparkles: Added function to fill master zone creation inputs

--- src/script.js
+++ src/script.js
@@ -30,3 +30,21 @@ const createEventListenerNewMasterZone = () => {
         }
     }, 100);
 }
+
+const createNewMasterZone = () => {
+    const interval = setInterval(() => {
+        const domainInput = window.frames[1].document.querySelector('#zone');
+        const masterInput = window.frames[1].document.querySelector('#master');
+        const emailInput = window.frames[1].document.querySelector('#email');
+        const ipInput = window.frames[1].document.querySelector('#ip');
+    
+        if (ipInput){
+            clearInterval(interval);
+    
+            masterInput.value = 'ns1.focomultimidia.com';
+            emailInput.value = 'fabricio.focomultimidia.com';
+            ipInput.value = '192.169.80.82';
+            domainInput.focus();
+        } 
+    }, 1000);
+}
