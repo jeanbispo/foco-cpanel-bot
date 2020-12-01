17d44838b2fdc070ae4724de2739bf90a0de1397
gcairesdev <pessoalgcaires@gmail.com>
2020-11-30 22:12:15 -0300

    :sparkles: Added event listener to identify if it is on the master zone page

--- src/script.js
+++ src/script.js
@@ -10,10 +10,23 @@ const waitingForServerLinkLoad = setInterval(() => {
 
 const start = () => {
     navigateToMainPage();
+    createEventListenerNewMasterZone();
 }
 
 const navigateToMainPage = () => {
     const linkToMainPage = window.frames[0].document.querySelector('#catservers a');
 
     linkToMainPage.click();
+}
+
+const createEventListenerNewMasterZone = () => {
+    const interval = setInterval(() => {
+        const zoneInput = window.frames[1].document.querySelector('#zone');
+    
+        if (zoneInput) {
+            clearInterval(interval);
+
+            createNewMasterZone();
+        }
+    }, 100);
 }
