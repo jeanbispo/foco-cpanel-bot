5f73f77b2747389f7e73d278f2c06e84b546e0ba
Guilherme Caires <pessoalgcaires@gmail.com>
2020-12-02 12:16:29 -0300

    :hammer: Better query selector to identify page

--- src/Bot/Navigation.js
+++ src/Bot/Navigation.js
@@ -4,19 +4,19 @@ export default function(Bot){
 
     Bot.loadPages = () => {
         Bot.pages = {
-            'system-information' : window.frames['right'].document.querySelector('[target=_new]'),
-            'general-options' : window.frames['right'].document.querySelector('#sortableTable12 a[href="conf_rndc.cgi"]'),
-            'create-master-zone' : window.frames['right'].document.querySelector('input#master'),
-            'edit-zone' : window.frames['right'].document.querySelector('#sortableTable1 [href="edit_recs.cgi?zone=testecpanel.com&view=any&type=A"'),
-            'address' : window.frames['right'].document.querySelector('#name'),
+            'system-information' : window.frames[1].document.querySelector('[target=_new]'),
+            'general-options' : window.frames[1].document.querySelector('#sortableTable12 a[href="conf_rndc.cgi"]'),
+            'create-master-zone' : window.frames[1].document.querySelector('input#master'),
+            'edit-zone' : window.frames[1].document.querySelector('#sortableTable1 [href="edit_recs.cgi?zone=testecpanel.com&view=any&type=A"'),
+            'address' : window.frames[1].document.evaluate("//font[contains(., 'Endereço Registros')]", document, null, XPathResult.ANY_TYPE, null ).iterateNext(),
         }
     }
 
     Bot.loadNavigateToPages = () => {
         Bot.navigateToPages = {
-            'general-options' : window.frames['left'].document.querySelector('[href="/bind8/"]'),
-            'create-master-zone' : window.frames['right'].document.querySelector('[href="master_form.cgi"]'),
-            'address' : window.frames['right'].document.querySelector('[href="edit_recs.cgi?zone=testecpanel.com&view=any&type=A"]'),
+            'general-options' : window.frames[0].document.querySelector('[href="/bind8/"]'),
+            'create-master-zone' : window.frames[1].document.querySelector('[href="master_form.cgi"]'),
+            'address' : window.frames[1].document.querySelector('[href="edit_recs.cgi?zone=testecpanel.com&view=any&type=A"]'),
         }
     }
 
