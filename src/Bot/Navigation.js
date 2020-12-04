5e1b601e516d72b3d93daf74e3ca83353f7dc8b3
Guilherme Caires <pessoalgcaires@gmail.com>
2020-12-04 20:42:27 -0300

    :hammer: Passed some responsibilities from Navigation.js to Page.js

--- src/Bot/Navigation.js
+++ src/Bot/Navigation.js
@@ -1,16 +1,5 @@
 export default function(Bot){
 
-    Bot.pages = {
-        'system-information' : '//font[contains(., "This login")]',
-        'general-options'    : '//font[contains(., "BIND 8")]',
-        'create-master-zone' : '//font[contains(., "Criar Zona Master")]',
-        'edit-zone'          : '//font[contains(., "Editar a Zona Master")]',
-        'address'            : '//font[contains(., "Endereço Registros")]',
-        'name-server'        : '//font[contains(., "Servidor de Nomes Registros")]',
-        'email-server'       : '//font[contains(., "Servidor de Email Registros")]',
-        'text'               : '//font[contains(., "Texto Registros")]',
-    }
-
     Bot.navigateToPages = {
         'general-options'    : '/bind8/',
         'create-master-zone' : '/bind8/master_form.cgi',
@@ -18,27 +7,5 @@ export default function(Bot){
     }
 
     Bot.navigate = page => window.location = Bot.navigateToPages[page];
-
-    Bot.waitForElement = (selector, callback, maxTimes = 6, time = 500) => {
-        const rightFrame = window.frames['right'];
-
-        const tryAgain = () => {
-            maxTimes--;
-            setTimeout(() => Bot.waitForElement(selector, callback, maxTimes), time);
-        }
-
-        if (rightFrame) {
-            const element = rightFrame.document
-                .evaluate(selector, rightFrame.document, null, XPathResult.ANY_TYPE, null).iterateNext();
-
-            return element ? callback() : tryAgain();
-        } else tryAgain();
-    }
-
-    Bot.doOnPage = (page, callback) => {
-        const documentSelector = Bot.pages[page];
-
-        return Bot.waitForElement(documentSelector, callback);
-    };
 
 }
--- src/Bot/Page.js
+++ src/Bot/Page.js
@@ -1,5 +1,16 @@
 export default function(Bot) { 
 
+    Bot.pages = {
+        'system-information' : '//font[contains(., "This login")]',
+        'general-options'    : '//font[contains(., "BIND 8")]',
+        'create-master-zone' : '//font[contains(., "Criar Zona Master")]',
+        'edit-zone'          : '//font[contains(., "Editar a Zona Master")]',
+        'address'            : '//font[contains(., "Endereço Registros")]',
+        'name-server'        : '//font[contains(., "Servidor de Nomes Registros")]',
+        'email-server'       : '//font[contains(., "Servidor de Email Registros")]',
+        'text'               : '//font[contains(., "Texto Registros")]',
+    }
+
     Bot.changePageTitle = title => document.title = title;
 
     Bot.changeFavicon = link => {
@@ -13,4 +24,27 @@ export default function(Bot) {
 
         document.head.appendChild(favicon);
     }
+
+    Bot.waitForElement = (selector, callback, maxTimes = 6, time = 500) => {
+        const rightFrame = window.frames['right'];
+
+        const tryAgain = () => {
+            maxTimes--;
+            setTimeout(() => Bot.waitForElement(selector, callback, maxTimes), time);
+        }
+
+        if (rightFrame) {
+            const element = rightFrame.document
+                .evaluate(selector, rightFrame.document, null, XPathResult.ANY_TYPE, null).iterateNext();
+
+            return element ? callback() : tryAgain();
+        } else tryAgain();
+    }
+
+    Bot.doOnPage = (page, callback) => {
+        const documentSelector = Bot.pages[page];
+
+        return Bot.waitForElement(documentSelector, callback);
+    };
+    
 }
