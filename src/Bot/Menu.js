073f651baac4c74487cb78916c14242a73fa0c3d
Guilherme Caires <pessoalgcaires@gmail.com>
2020-12-04 20:57:44 -0300

    :hammer: Refactored menu creation

--- src/Bot/Menu.js
+++ src/Bot/Menu.js
@@ -16,47 +16,65 @@ export default function(Bot) {
 
     Bot.createBotMenu = () => {
         const interval = setInterval(() => {
-            const frameLeft = window.frames['left'].document.querySelector('#popup');
+           const popup = window.frames['left'].document.querySelector('#popup');
             
-            if (frameLeft) {
+            if (popup) {
                 clearInterval(interval);
 
-                const avatar = document.createElement('img');
-                avatar.src = 'https://i.ibb.co/tBckqzd/avatar.png';
-                avatar.onclick = () => {
-                    window.open('https://sdn01fmu.focomultimidia.com:10000/');
-                    window.close();
-                }
+                Bot.menu = document.createElement('div');
+                Bot.menu.classList.add('bot-menu');
 
-                const speachBoubble = document.createElement('div');
-                speachBoubble.classList.add('speach-boubble');
-                speachBoubble.innerText = 'Crie uma zona master no campo abaixo!';
-
-                const domainInput = document.createElement('input');
-                domainInput.type = 'text';
-                domainInput.name = 'domain';
-                domainInput.autofocus = true;
-                domainInput.placeholder = 'dominio.com.br';
+                Bot.addAvatar();
+                Bot.addSpeachBoubble();
+                Bot.addForm();
+                Bot.addLogout();
                 
-                const form = document.createElement('form');
-                form.onsubmit = (event) => Bot.submitDomain(event);
-                form.appendChild(domainInput);
-
-                const logoutLink = document.createElement('a');
-                logoutLink.href = '/session_login.cgi?logout=1';
-                logoutLink.innerText = 'Sair';
-                logoutLink.id = 'logout';
-                
-                const menu = document.createElement('div');
-                menu.classList.add('bot-menu');
-                menu.appendChild(avatar);
-                menu.appendChild(speachBoubble);
-                menu.appendChild(form);
-                menu.appendChild(logoutLink);
-
-                frameLeft.appendChild(menu);
+                popup.appendChild(Bot.menu);
             }
         }, 2000);
+    }
+
+    Bot.addAvatar = () => {
+        const avatar = document.createElement('img');
+        avatar.src = 'https://i.ibb.co/tBckqzd/avatar.png';
+        avatar.onclick = () => {
+            window.open('https://sdn01fmu.focomultimidia.com:10000/');
+            window.close();
+        }
+
+        Bot.menu.appendChild(avatar);
+    }
+
+    Bot.addSpeachBoubble = () => {
+        const speachBoubble = document.createElement('div');
+        speachBoubble.classList.add('speach-boubble');
+        speachBoubble.innerText = 'Crie uma zona master no campo abaixo!';
+
+        Bot.menu.appendChild(speachBoubble);
+    }
+
+    Bot.addForm = () => {
+        const form = document.createElement('form');
+        const domainInput = document.createElement('input');
+
+        domainInput.type = 'text';
+        domainInput.name = 'domain';
+        domainInput.autofocus = true;
+        domainInput.placeholder = 'dominio.com.br';
+                
+        form.onsubmit = (event) => Bot.submitDomain(event);
+        form.appendChild(domainInput);
+
+        Bot.menu.appendChild(form);
+    }
+
+    Bot.addLogout = () => {
+        const logoutLink = document.createElement('a');
+        logoutLink.href = '/session_login.cgi?logout=1';
+        logoutLink.innerText = 'Sair';
+        logoutLink.id = 'logout';
+
+        Bot.menu.appendChild(logoutLink);
     }
 
     Bot.submitDomain = event => {
